e7ef846bb3fa93e0ac33aeb984afa156bb7472e3
elianagsilva <elianacrisa60@gmail.com>
2022-07-19 15:11:55 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+let comedy = [
+    {
+        title: "Meet the parents",
+        director: "Jay Roach",
+        year: 200
+    },
+    {
+        title: "Mean Girls",
+        director: "Mark Waters",
+        year: 2004
+    },
+    {
+        title: "The trip",
+        director: "Michael Winterbottom",
+        year: 2011
+    }
+]
+
+let action =  [
+    {
+        title: "Captain America: Civil War",
+        director: "Anthony Russo",
+        year: 2016
+    },
+    {
+        title: "Spider-Man: Homecoming",
+        director: "Jon Watts",
+        year: 2017
+    },
+    {
+        title: "Star Wars: The Rise of Skywalker ",
+        director: "J. J. Abrams",
+        year: 2019
+    }
+]
+
+let fantasy =  [
+    {
+        title: "Edward scissorhands",
+        director: "Cath Clarke",
+        year: 1991
+    },
+    {
+        title: "Willy Wonka and the Chocolate Factory",
+        director: "David Calhoun",
+        year: 1971
+    },
+    {
+        title: "Labyrinth",
+        director: "Tom Huddleston",
+        year: 1986
+    }
+]
